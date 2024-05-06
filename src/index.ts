7447136825abc30ea7e7ee496bd6c27e0307a591
Marcos Rodríguez <e.marcosra@gmail.com>
2024-05-06 19:47:38 +0200

    Create listener and endpoint

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
+
+app.use((_req, res) => {
+  res.status(404).json({ error: "Endpoint not found" });
+});
