aff249c68a32280eabb62b21dfdf18ca5d42f052
Marcos Rodríguez <e.marcosra@gmail.com>
2024-05-07 21:21:50 +0200

    Add ping middleware

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.listen(5000, () => {
-  console.log("Listening on port 5000");
-});
-app.use((_req, res) => {
-  res.status(404).json({ error: "Endpoint not found" });
-});
-//# sourceMappingURL=index.js.map
